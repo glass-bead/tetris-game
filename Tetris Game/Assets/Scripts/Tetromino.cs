8f5fd6b779bad7003eba223e86d99c33eeaf0826
Carolina Veloso <carolina.mg.veloso@gmail.com>
2023-04-21 11:28:08 +0100

    Tetromino logic - falling

--- Tetris Game/Assets/Scripts/Tetromino.cs
+++ Tetris Game/Assets/Scripts/Tetromino.cs
@@ -1,18 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Tetromino : MonoBehaviour
 {
+    private const float fallTime = 0.8f;
+    private float previousTime = fallTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Falling();
     }
+
+    private void Falling()
+    {
+        // Makes the tetromino fall. Accelerates movement with user input.
+        if (Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 20 : fallTime))
+        {
+            transform.position += new Vector3(0, -1, 0);
+            previousTime = Time.time;
+        }
+    }
+
 }
