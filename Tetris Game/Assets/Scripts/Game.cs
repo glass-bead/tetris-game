7a771971790e0ae507d14ad260a70921dd6a60a8
Carolina Veloso <carolina.mg.veloso@gmail.com>
2023-04-21 10:51:41 +0100

    Tetromino logic - spawning

---
+++ Tetris Game/Assets/Scripts/Game.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public GameObject[] tetrominoList;
+    private GameObject tetromino;
+
+    private Vector3 spawnPos = new(-4f, 8f, 0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TODO
+    }
+
+    // Spawn tetromino on the board
+    private void Spawn() {
+
+        // Select random tetromino to spawn from tetrominos' list
+        int spawnIndex = Random.Range(0, 6);
+
+        // Instantiate tetromino
+        var currTetromino = Instantiate(tetrominoList[spawnIndex], spawnPos, Quaternion.identity);
+        currTetromino.AddComponent<Tetromino>();
+    }
+}
