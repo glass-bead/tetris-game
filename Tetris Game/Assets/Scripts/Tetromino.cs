987314c7bce86412d51e775c3a513a7e7b411512
Carolina Veloso <carolina.mg.veloso@gmail.com>
2023-04-21 15:22:53 +0100

    Tetromino logic - handle user input
    Handles user input for left, right and down movement

--- Tetris Game/Assets/Scripts/Tetromino.cs
+++ Tetris Game/Assets/Scripts/Tetromino.cs
@@ -1,30 +1,69 @@
+using System;
 using UnityEngine;
 
 public class Tetromino : MonoBehaviour
 {
-    private const float fallTime = 0.8f;
-    private float previousTime = fallTime;
+    private const float fallDelay = 0.8f;
+    private const float moveDelay = 0.1f;
+    private float fallTime;
+    private float moveTime;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        moveTime = Time.time + moveDelay;
+        fallTime = Time.time + fallDelay;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Falling();
+        // Allow the player to hold movement keys but with move delay
+        if (Time.time > moveTime)
+        {
+            HandleUserInputs();
+        }
+
+        // Move tetromino down every x seconds
+        if (Time.time > fallTime && !Input.GetKey(KeyCode.DownArrow))
+        {
+            Falling();
+        }
+    }
+
+    private void HandleUserInputs()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+            moveTime = Time.time + moveDelay;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+            moveTime = Time.time + moveDelay;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow)) 
+        {
+            transform.position += new Vector3(0, -1, 0);
+            moveTime = Time.time + moveDelay / 2;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Rotate();
+        }
+
     }
 
     private void Falling()
     {
-        // Makes the tetromino fall. Accelerates movement with user input.
-        if (Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 20 : fallTime))
-        {
-            transform.position += new Vector3(0, -1, 0);
-            previousTime = Time.time;
-        }
+        transform.position += new Vector3(0, -1, 0);
+        fallTime = Time.time + fallDelay;
     }
 
+    private void Rotate()
+    {
+        throw new NotImplementedException();
+    }
 }
